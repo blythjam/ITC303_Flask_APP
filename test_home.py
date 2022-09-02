e2f9e6176cab684e3a4c91cc0f70e210474e7948
Jamie Blyth <blythjamie@gmail.com>
2022-09-02 14:00:22 +1000

    fixed test

--- test_home.py
+++ test_home.py
@@ -1,10 +1,10 @@
 from logging import makeLogRecord
 from typing_extensions import assert_type
 from urllib import response
-import LicensePlateDetection
+from .LicensePlateDetection import app
 
 def test_paid_page():
-    flask_app = LicensePlateDetection.app
+    flask_app = app
     
     with flask_app.test_client() as test_client:
         response = test_client.get('/home')       
